88204c1f145774ca8e229fdd922c4dcb5753deba
kangangi <wanjikukangangi@gmail.com>
2020-02-29 19:15:31 +0300

    add size logic to js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,26 @@ function Order(name, size, type){
   this.type = [];
 }
 
-function pizzaType(pizzaFlavour, crust, toppings){
+function PizzaType(pizzaFlavour, crust, toppings){
   this.pizzaFlavour = pizzaFlavour;
   this.crust = crust;
   this.toppings = toppings;
 }
+
+$(document).ready(function(){
+  $("form#order").submit(function(event){
+    event.preventDefault();
+
+    var inputtedName = $("input#name").val();
+    var inputtedSize = $("input:radio[name=size]:checked").val();
+
+    if (inputtedSize ==="small"){
+      var sizeCost = 500;
+    } else if(inputtedSize ==="medium"){
+      var sizeCost = 750;
+    }else{
+      var sizeCost = 1000;
+    }
+    alert(sizeCost);
+  });
+});
