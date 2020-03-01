5a5cd73b26f8189ea022519ef32f0e7227fea8f1
kangangi <wanjikukangangi@gmail.com>
2020-03-01 19:12:57 +0300

    add changes to add another pizza function

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function Order(name,type,total){
   this.total = total;
 }
 
-function PizzaType(pizzaFlavor, size, crust, toppings){
+function PizzaType(pizzaFlavor, size, crust){
   this.size = size;
   this.pizzaFlavor = pizzaFlavour;
   this.crust = crust;
@@ -12,6 +12,35 @@ function PizzaType(pizzaFlavor, size, crust, toppings){
 }
 
 $(document).ready(function(){
+  $("#add-another-pizza").click(function(){
+    $("#new-order").append('<br>'+'<p>Select a size:</p>'+'<div class="row">' + '<div class="col-md-4">'+
+        '<div class="form-check">' + '<input class="form-check-input" type="radio" name="size" id="small" value="small" >'+
+        '<label class="form-check-label" for="small">Small</label>' + '</div>'+'</div>' +
+        '<div class="col-md-4">'+'<div class="form-check">'+'<input class="form-check-input" type="radio" name="size" id="medium" value="medium">'+
+        '<label class="form-check-label" for="medium">Medium</label>'+'</div>'+'</div>'+
+        '<div class="col-md-4">'+'<div class="form-check">'+'<input class="form-check-input" type="radio" name="size" id="large" value="large">'+
+        '<label class="form-check-label" for="large">Large</label>'+'</div>'+'</div>'+'</div>'+
+        '<p>Select type of pizza</p>'+'<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="margherita" value="margherita" >'+
+        '<label class="form-check-label" for="margherita">Margherita</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="marinara"  value="marinara" >'+'<label class="form-check-label" for="marinara">Marinara</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="bbqchicken" value="bbqchicken" >'+'<label class="form-check-label" for="bbqchicken">Barbecue Chicken</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="bbqbeef" value="bbqbeef" >'+'<label class="form-check-label" for="bbqbeef">Barbecue Beef</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="hawaiian" value="hawaiian"  >'+'<label class="form-check-label" for="hawaiian">Hawaiian</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="sweet-chilli-chicken" value="sweet-chilli-chicken"  >'+'<label class="form-check-label" for="sweet-chilli-chicken">Sweet Chilli Chicken</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="original-veggie" value="original-veggie"  >'+'<label class="form-check-label" for="original-veggie">Original Veggie</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="chicken-tikka" value="chicken-tikka" >'+'<label class="form-check-label" for="chicken-tikka">Chicken Tikka</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="meat-fest" value="meat-fest" >'+'<label class="form-check-label" for="meat-fest">Meat fest</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="radio" name="pizza" id="pork-rib" value="pork-rib" >'+'<label class="form-check-label" for="pork-rib">Pork Rib</label>'+'</div>'+'<br>'+
+        '<p>Select type of crust</p>'+'<select class="form-control" id="crust">'+
+        '<option value="0">Thin Crust</option>'+'<option value="1">Thick Crust</option>'+'<option value="2">Glutten Free</option>'+'<option value="3">Cheese Stuffing</option>'+'</select>'+'<br>'+
+        '<p>Select your choice of extra toppings</p>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="checkbox" name="toppings" id="bacon" >'+'<label class="form-check-label" for="bacon">Bacon</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="checkbox" name="toppings" id="peporoni" >'+'<label class="form-check-label" for="peporoni">Peporoni</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="checkbox" name="toppings" id="cheese" >'+'<label class="form-check-label" for="cheese">Cheese</label>'+'</div>'+
+        '<div class="form-check">'+'<input class="form-check-input" type="checkbox" name="toppings" id="onions" >'+'<label class="form-check-label" for="onions">Onions</label>'+'</div>'+'</div>'
+      )
+      });
+  });
   $("form#order").submit(function(event){
     event.preventDefault();
 
@@ -19,6 +48,7 @@ $(document).ready(function(){
     var inputtedSize = $("input:radio[name=size]:checked").val();
     var inputtedFlavor =$("input:radio[name=pizza]:checked").val();
     var inputtedCrust =$("#crust").val();
+
 
     if (inputtedSize ==="small"){
       var sizeCost = 500;
@@ -114,5 +144,4 @@ $(document).ready(function(){
         break;
       };
     };*/
-  });
 });
